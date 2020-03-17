5e336d097284b22ab638a7302a13cd9a45ac3aa3
Vitalik Kim <kimv1086@gmail.com>
2020-03-17 15:41:21 +0600

    HW#86 - added user page

--- frontend/src/components/UI/Menus/AnonimousMenu.js
+++ frontend/src/components/UI/Menus/AnonimousMenu.js
@@ -3,14 +3,14 @@ import {NavItem, NavLink} from "reactstrap";
 import {NavLink as RouterNavLink} from "react-router-dom";
 
 const AnonymousMenu = () => (
-    <Fragment>
-        <NavItem>
-            <NavLink tag={RouterNavLink} to="/register" exact>Sign up</NavLink>
-        </NavItem>
-        <NavItem>
-            <NavLink tag={RouterNavLink} to="/login" exact>Login</NavLink>
-        </NavItem>
-    </Fragment>
+  <Fragment>
+    <NavItem>
+      <NavLink tag={RouterNavLink} to="/register" exact>Sign up</NavLink>
+    </NavItem>
+    <NavItem>
+      <NavLink tag={RouterNavLink} to="/login" exact>Login</NavLink>
+    </NavItem>
+  </Fragment>
 );
 
 export default AnonymousMenu;
--- frontend/src/components/UI/Menus/UserMenu.js
+++ frontend/src/components/UI/Menus/UserMenu.js
@@ -1,48 +1,21 @@
-import React, {Fragment} from 'react';
-import {DropdownItem, DropdownMenu, DropdownToggle, NavItem, NavLink, UncontrolledDropdown} from "reactstrap";
-import {NavLink as RouterNavLink} from "react-router-dom";
-import {apiURL} from "../../../constants";
-
-
-const style={
-    width: "50px",
-    height: '50px',
-    borderRadius: '50%'
-};
+import React from 'react';
+import {DropdownItem, DropdownMenu, DropdownToggle, UncontrolledDropdown} from "reactstrap";
 
 const UserMenu = ({user, logout}) => (
-    <Fragment>
-        <NavItem>
-            <NavLink tag={RouterNavLink} to="/trackHistory" exact>TrackHistory</NavLink>
-        </NavItem>
-        <NavItem>
-            <NavLink tag={RouterNavLink} to="/artists/new" exact>Add new artist</NavLink>
-        </NavItem>
-        <NavItem>
-            <NavLink tag={RouterNavLink} to="/albums/new" exact>Add new album</NavLink>
-        </NavItem>
-        <NavItem>
-            <NavLink tag={RouterNavLink} to="/tracks/new" exact>Add new track</NavLink>
-        </NavItem>
-        <UncontrolledDropdown nav inNavbar>
-            <DropdownToggle nav caret>
-                {user.facebookId ?
-                    <img src={user.avatarImage} alt="avatar" style={style}/>
-                    : <img src={apiURL + '/uploads/' + user.avatarImage} alt="avatar"   style={style}/>
-                }
-                Hello, {user.displayName}
-            </DropdownToggle>
-            <DropdownMenu right>
-                <DropdownItem>
-                    Show profile
-                </DropdownItem>
-                <DropdownItem divider/>
-                <DropdownItem onClick={logout}>
-                    Log out
-                </DropdownItem>
-            </DropdownMenu>
-        </UncontrolledDropdown>
-    </Fragment>
+  <UncontrolledDropdown nav inNavbar>
+    <DropdownToggle nav caret>
+      Hello, {user.username}
+    </DropdownToggle>
+    <DropdownMenu right>
+      <DropdownItem>
+        Show profile
+      </DropdownItem>
+      <DropdownItem divider/>
+      <DropdownItem onClick={logout}>
+        Log out
+      </DropdownItem>
+    </DropdownMenu>
+  </UncontrolledDropdown>
 );
 
 export default UserMenu;
